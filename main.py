a62d366b3562644def1ff4d5c2dbed668b871656
jsolano0112 <juana.solano@correo.tdea.edu.co>
2023-04-28 11:44:57 -0500

    agregando conexion a bd

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import psycopg2
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+conexion = psycopg2.connect(
+    user = 'postgres',
+    password = 'Admin',
+    host = '127.0.0.1',
+    port = '5432',
+    database = 'test_db'
+)
+print(conexion)
